9fd04909b7a34ab08d1738c2a5789120c86cf84e
Collins Agbonghama <Collizo4sky@gmail.com>
2023-07-13 08:47:57 +0100

    Create subscribe-users-to-free-plan-after-paid-plan-subscription-expiration.php

---
+++ subscribe-users-to-free-plan-after-paid-plan-subscription-expiration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Subscribe customers to a free plan (with plan ID 13) after subscription to paid plans (10, 11, 12) expires
+ */
+add_action('ppress_subscription_expired', function(SubscriptionEntity $sub) {
+    if(in_array($sub->get_id(), [10, 11, 12])) {
+        ppress_subscribe_user_to_plan(13, $sub->get_customer_id());
+    }
+});
