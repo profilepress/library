110595ab6b9e131879a1c20f2fd638e6d80c4118
Ibrahim Nasir <kh4l33l@yahoo.com>
2023-10-06 04:00:35 +0100

    Update add-metered-paywall-support-check-to-logged-in-users.php
    Fixed a typo in support

--- add-metered-paywall-support-check-to-logged-in-users.php
+++ add-metered-paywall-support-check-to-logged-in-users.php
@@ -1,3 +1,3 @@
 <?php
 /* Give logged-in users free views from metered paywall addon */
- add_filter('ppress_metered_paywall_logged_in_user_check_suport', '__return_true');
+ add_filter('ppress_metered_paywall_logged_in_user_check_support', '__return_true');
