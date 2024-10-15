5b5ce035b69b8ab8726278115936cdd67fdd2e56
Collins Agbonghama <Collizo4sky@gmail.com>
2024-10-15 14:19:36 +0100

    Update social-login-specific-login-redirect.php

--- social-login-specific-login-redirect.php
+++ social-login-specific-login-redirect.php
@@ -2,7 +2,19 @@
 /* 
 * Specify a url to redirect your users who only log in or signup to your site via social login
 */
-
 add_filter('ppress_social_login_redirect', function() {
     return 'https://url-to-redirect-to-here.com';
 });
+
+/* 
+* Specify a url to redirect users who is just registered via social login after they are auto-logged in
+*/
+add_filter('ppress_social_login_redirect', function($social_login_redirect, $provider, $user_id, $registration_flag) {
+
+    if($registration_flag) {
+        $social_login_redirect = 'https://url-to-redirect-to-here.com';
+    }
+
+    return $social_login_redirect;
+
+}, 10, 4);
