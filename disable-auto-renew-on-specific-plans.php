00cf5a4a637119bc587e8a575e7280228f7effcd
Ibrahim Nasir <kh4l33l@yahoo.com>
2025-04-11 11:53:31 +0100

    Fixes

--- disable-auto-renew-on-specific-plans.php
+++ disable-auto-renew-on-specific-plans.php
@@ -1,7 +1,7 @@
 <?php
 /** Disable auto renewal on membership plans with the IDs in the array */
 add_filter('ppress_subscription_is_auto_renew', function($result, $planEntity) {
-  if(in_array($planEntity->get_id(), [33, 31, 30, 36, 26])) {
+  if(in_array($planEntity->get_id(), [33, 31])) { // Please replace 33, 31 with your plan IDs
     return false;
   }
   return $result;
